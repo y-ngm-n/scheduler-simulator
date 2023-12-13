a85d98d61fba2f876c10db04d155b76bc933e1d6
y-ngm-n <wogitopp@naver.com>
2023-12-13 21:20:15 +0900

    5. IO Queue

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #define TQ 4  // time quantum
 #define TT 1  // time tick
 
-void child_process(int i, int cpuBurst);
+void child_process(int i, int cpuBurst, int ioBurst, int ioStart);
 void processSwitching();
 void processTerminating();
 void schedulerTerminating();
@@ -24,7 +24,7 @@ void determiningIO(int cpuBurst, int &start, int &burst);
 // IPC message
 struct Message {
   long type;
-  int flag;
+  int flag;  // 0: terminal flag, 1: IO start flag, 2: IO end flag
 };
 // PCB
 struct PCB {
@@ -36,6 +36,7 @@ struct PCB {
 };
 
 vector<PCB*> readyQueue;
+vector<PCB*> ioQueue;
 int idx = 1;
 int curTQ = 0;
 int msg_queue_p;
@@ -43,8 +44,9 @@ int msg_queue_c;
 
 
 // child process
-void child_process(int i, int cpuBurst) {
+void child_process(int i, int cpuBurst, int ioBurst, int ioStart) {
   Message parentMsg, childMsg;
+  bool state = true;  // if 1: ready queue, 0: IO queue
 
   // message queue
   int msg_queue_p = msgget((key_t)MSG_KEY_P, 0666|IPC_CREAT);
@@ -58,22 +60,44 @@ void child_process(int i, int cpuBurst) {
     exit(EXIT_FAILURE);
   }
 
-  sleep(2);
+  sleep(1);
 
   // recieve message
   while(true) {
-    if (msgrcv(msg_queue_p, &parentMsg, sizeof(Message)-sizeof(long), i, IPC_NOWAIT)==-1) continue;
-    if (cpuBurst>0) cpuBurst--;
-    if (cpuBurst==0) {
-      childMsg.type = i; childMsg.flag = 1;
-      if (msgsnd(msg_queue_c, &childMsg, sizeof(Message)-sizeof(long), 0)==-1) {
-        perror("msgsnd");
-        exit(EXIT_FAILURE);
+    if (!state) {
+      sleep(1);
+      if (ioBurst>0) ioBurst--;
+      cout << i << ": " << ioBurst << endl;
+      if (ioBurst==0) {
+        childMsg.type = i; childMsg.flag = 2;
+        if (msgsnd(msg_queue_c, &childMsg, sizeof(Message)-sizeof(long), 0)==-1) {
+          perror("msgsnd");
+          exit(EXIT_FAILURE);
+        }
+        state = true;
+        continue;
       }
-      // cout << "  [CHILD] #" << i << " " << getpid() << " ended" << endl;
-      exit(0);
     }
-    // cout << "remained CPU " << i << ": " << cpuBurst << endl;
+    if (msgrcv(msg_queue_p, &parentMsg, sizeof(Message)-sizeof(long), i, IPC_NOWAIT)==-1) continue;
+    if (state==true) {
+      if (cpuBurst>0) cpuBurst--;
+      if (cpuBurst==0) {
+        childMsg.type = i; childMsg.flag = 0;
+        if (msgsnd(msg_queue_c, &childMsg, sizeof(Message)-sizeof(long), 0)==-1) {
+          perror("msgsnd");
+          exit(EXIT_FAILURE);
+        }
+        exit(0);
+      }
+      if (cpuBurst==ioStart) {
+        childMsg.type = i; childMsg.flag = 1;
+        if (msgsnd(msg_queue_c, &childMsg, sizeof(Message)-sizeof(long), 0)==-1) {
+          perror("msgsnd");
+          exit(EXIT_FAILURE);
+        }
+        state = false;
+      }
+    }
   }
 }
 
@@ -91,7 +115,8 @@ void processSwitching() {
 void processTerminating() {
   if (readyQueue.size()==1) {
     readyQueue.erase(readyQueue.begin());
-    schedulerTerminating();
+    if (!ioQueue.size()) schedulerTerminating();
+    return;
   }
   curTQ = 0;
   // cout << "[ terminate: " << readyQueue[0]->num;
@@ -109,21 +134,50 @@ void schedulerTerminating() {
 void printReadyQueue() {
   cout << "Ready Queue: [ ";
   for (auto process : readyQueue) {
-    cout << process->num << "(" << process->pid << "): " << process->cpuBurst << "s";
-    if (*(readyQueue.end())!=process) cout << ", ";
+    cout << process->num << "(" << process->pid << "): " << process->cpuBurst << "s, ";
+    // if (process!=*(readyQueue.end())) cout << ", ";
+  }
+  cout << " ]" << endl;
+}
+
+void printIOQueue() {
+  cout << "IO Queue: [ ";
+  for (auto process : ioQueue) {
+    cout << process->num << "(" << process->pid << "): " << process->ioBurst << "s";
+    if (process!=*(ioQueue.end())) cout << ", ";
   }
   cout << " ]" << endl;
 }
 
 void determiningIO(int cpuBurst, int &start, int &burst) {
-  srand((unsigned int)time(NULL));
-  bool flag = rand()%2;
-  if (flag) {
+  srand((unsigned int)time(NULL)*cpuBurst);
+  int flag = rand()%3;
+  if (cpuBurst==1) { start = 0; burst = 0; }
+  else if (flag) {
     start = (rand()%(cpuBurst-1)) + 1;
     burst = (rand()%5) + 1;
   }
   else { start = 0; burst = 0; }
   return;
+}
+
+void readyToIO() {
+  curTQ = 0;
+  ioQueue.push_back(readyQueue[0]);
+  readyQueue.erase(readyQueue.begin());
+  if (readyQueue.size()) idx = readyQueue[0]->num;
+  else idx = 0;
+}
+
+void ioToReady(int num) {
+  vector<PCB*>::iterator it;
+  it = ioQueue.begin();
+  while(true) {
+    if ((*it)->num==num) break;
+    it++;
+  }
+  readyQueue.push_back(*it);
+  ioQueue.erase(it);
 }
 
 
@@ -134,8 +188,17 @@ void timerHandler(int signum) {
   parentMsg.type = idx; parentMsg.flag = 0;
 
   if (msgrcv(msg_queue_c, &childMsg, sizeof(Message)-sizeof(long), 0, IPC_NOWAIT)!=-1) {
-    processTerminating();
-    parentMsg.type = idx;
+    if (childMsg.flag==0) {
+      processTerminating();
+      parentMsg.type = idx;
+    }
+    else if (childMsg.flag==1) {
+      readyToIO();
+      parentMsg.type = idx;
+    }
+    else if (childMsg.flag==2) {
+      ioToReady(childMsg.type);
+    }
   }
   else if (curTQ==TQ) {
     processSwitching();
@@ -144,6 +207,10 @@ void timerHandler(int signum) {
 
   readyQueue[0]->cpuBurst--;
   printReadyQueue();
+  printIOQueue();
+  cout << endl;
+
+  for (auto io : ioQueue) io->ioBurst--;
 
   if (msgsnd(msg_queue_p, &parentMsg, sizeof(Message)-sizeof(long), 0)==-1) {
     perror("msgsnd");
@@ -156,6 +223,7 @@ void timerHandler(int signum) {
 
 int main() {
   pid_t ppid=getpid();
+  readyQueue.clear(); ioQueue.clear();
   for (int i=0; i<N; i++) readyQueue.push_back(new PCB());
 
   // create child processes
@@ -167,7 +235,7 @@ int main() {
     readyQueue[i]->pid = pid;
 
     // determining CPU burst time
-    srand((unsigned int)time(NULL)*i);
+    srand((unsigned int)time(NULL)+(2*i));
     int n = (rand()%10) + 1;
     readyQueue[i]->cpuBurst = n;
 
@@ -184,11 +252,10 @@ int main() {
       cout << "[PARENT] created #" << i+1 << " " << readyQueue[i]->num << " -> " << readyQueue[i]->cpuBurst << endl;
     }
     // child process
-    else child_process(i+1, readyQueue[i]->cpuBurst);
+    else child_process(i+1, readyQueue[i]->cpuBurst, readyQueue[i]->ioBurst, readyQueue[i]->ioStart);
   }
   cout << endl;
 
-  sleep(1);
 
   // signal handler
   struct sigaction action;
